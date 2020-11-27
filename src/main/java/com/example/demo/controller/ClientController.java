9ef0d46bc44d8f0d8e7234111743a8ea40ff504b
p3tzata <p3tzata_@abv.bg>
2020-11-27 16:15:33 +0200

    2020-11-27

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
@@ -11,9 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.domainEntity.Customer;
 import com.example.demo.repository.CustomerRepo;
+/*
+Overriding Spring Data REST Response Handlers
+Sometimes you may want to write a custom handler for a specific resource. 
+To take advantage of Spring Data REST’s settings, message converters, exception handling, and more.
+This add prefix spring.data.rest.base-path, but returned value from handler methods doesn't write response 
+to body, we should return ResponseEntity. 
 
-//This add prefix spring.data.rest.base-path, but returned value from handler methods doesn't write response 
-//to body, we should return ResponseBody.
+*/
 @RepositoryRestController
 public class ClientController {
 	
