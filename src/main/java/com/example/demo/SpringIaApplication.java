5887ca3b95d468c5b820cf45a3665e8f23b52e65
p3tzata <p3tzata_@abv.bg>
2021-02-11 14:31:03 +0200

    2021-02-11

--- src/main/java/com/example/demo/SpringIaApplication.java
+++ src/main/java/com/example/demo/SpringIaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringIaApplication {
 
 	public static void main(String[] args) {
